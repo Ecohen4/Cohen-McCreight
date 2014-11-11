c21d6874ca41d4bed4d35b0d726a4a91427976e3
Ecohen4 <ec2977@columbia.edu>
2014-11-11 14:14:25 -0500

    no changes to NOAA_ISD.R

--- NOAA_ISD.R
+++ NOAA_ISD.R
@@ -1,5 +1,16 @@
-# The following libraries will be required for this script
+## The following function will load the packages required for this tutorial.  If a package cannot be found in your instance of Rstudio, it will automatically be insalled.
+load_install<-function(lib){
+  if(! require(lib, character.only=TRUE)) install.packages(lib, character.only=TRUE)
+  library(lib, character.only=TRUE)
+}
 
+## the required libraries (e.g. packages)
+Thelib<-c("knitr", "plyr", "ggplot2", "scales", "gdata", "reshape", "reshape2", "ggmap", "FNN", "gridExtra", "xlsx")
+
+## apply the function
+lapply(Thelib, load_install)
+
+# The following libraries will be required for this script
 require(ggmap)
 require(FNN)
 require(reshape)
@@ -22,7 +33,7 @@ weather.stations <- read.csv("isd_weather_station_history.csv")
 # For ease, we'll change the names of a few columns
 colnames(weather.stations)[c(3, 9)] <- c("NAME", "ELEV")
 
-# Now take out all weather stations that have NAs for LAT & LON 
+# Now take out all weather stations that have NAs for LAT & LON
 # This will eiliminate 1,311 stations
 weather.stations <- weather.stations[!is.na(weather.stations$LAT) & !is.na(weather.stations$LON),]
 
@@ -32,7 +43,7 @@ weather.stations <- weather.stations[!is.na(weather.stations$LAT) & !is.na(weath
 
 # Ready to start getting specific weather stations.
 # First create a list of cities of interest:
-cities.of.interest <- c("New York City", "Philadelphia")
+cities.of.interest <- c("Sao Vicente", "Mindelo")
 
 
 
@@ -48,20 +59,20 @@ k.nearest.stations <- function(city, k)
   coordinates.of.interest <- geocode(city)
   nearest.stations <- get.knnx(as.matrix(weather.stations[,c(8,7)]),
                                as.matrix(coordinates.of.interest), k)
-  
+
   stations.of.interest <- weather.stations[nearest.stations$nn.index[,],]
-  
+
   stations.of.interest$rank <- rep(seq(from=1,to=nrow(coordinates.of.interest) ,by=1),
                                    nrow(stations.of.interest)/nrow(coordinates.of.interest))
-  
+
   stations.of.interest$city <- rep(city,nrow(stations.of.interest)/nrow(coordinates.of.interest))
-  
+
   stations.of.interest <- stations.of.interest[order(stations.of.interest$city), ]
-  
+
   stations.of.interest$BEGIN_Date <- as.numeric(substr(stations.of.interest$BEGIN, 1, 4)) # keep original dates, use new column
   stations.of.interest$END_Date <- as.numeric(substr(stations.of.interest$END, 1, 4))
-  
-  assign("stations.of.interest", stations.of.interest, envir = .GlobalEnv) 
+
+  assign("stations.of.interest", stations.of.interest, envir = .GlobalEnv)
 } # End function
 k.nearest.stations(cities.of.interest, 5)
 
@@ -74,18 +85,18 @@ weather.data <- function(city, beg.date, end.date)
   colnames(temp.log) <- c("File","DL Status", "City")
   temp.log$City <- st.list$city
   output.log <- data.frame()
-  
+
   for (year in beg.date:end.date)
   {
     for (i in 1:nrow(temp.log))
     {
-      temp.log[i, 1] <- paste(sprintf("%06d", st.list[i,1]), "-", 
+      temp.log[i, 1] <- paste(sprintf("%06d", st.list[i,1]), "-",
                               sprintf("%05d", st.list[i,2]), "-", year,
                               ".gz", sep = "")
-      
+
       tryCatch(
       {
-          download.file(paste("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/", year, "/", 
+          download.file(paste("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/", year, "/",
                       temp.log[i, 1], sep = ""),paste(temp.log[i,1]))
           temp.log[i,2] <- "Success"
       },
@@ -100,12 +111,12 @@ weather.data <- function(city, beg.date, end.date)
 
       output.log <- rbind(output.log, temp.log) # Combine each year's log
       output.log <- output.log[order(output.log$File),]
-    
-      assign("output.log", output.log, envir = .GlobalEnv) 
 
-  } # end year 
+      assign("output.log", output.log, envir = .GlobalEnv)
 
-write.csv(output.log, paste("output_log", beg.date, end.date, 
+  } # end year
+
+write.csv(output.log, paste("output_log", beg.date, end.date,
                             paste("(cities:",length(city),")",sep=""),
                             format(Sys.time(), "%s"), sep="_"), row.names=F)
 } # End function
@@ -122,11 +133,11 @@ file.conversions <- function()
                      7, 5, 5, 5, 4, 3, 1, 1, 4, 1,
                      5, 1, 1, 1, 6, 1, 1, 1, 5, 1,
                      5, 1, 5, 1) # I need to understand what all of these represent...
-  
+
   stations <- as.data.frame(matrix(NA, length(files),6))
   colnames(stations) <- c("USAFID", "WBAN", "YR", "LAT", "LONG", "ELEV")
-  
-  for (i in 1:length(files)) 
+
+  for (i in 1:length(files))
   {
     tryCatch({
       data <- read.fwf(gzfile(files[i]), column.widths)
@@ -136,13 +147,13 @@ file.conversions <- function()
                        "WIND.DIR", "WIND.SPD", "TEMP", "DEW.POINT",
                        "ATM.PRES")
     },
-    
+
     error=function(cond)
     {
       return(NULL)
       next
     })
-    
+
     data$LAT <- data$LAT/1000
     data$LONG <- data$LONG/1000
     data$WIND.SPD <- data$WIND.SPD/10
@@ -154,9 +165,9 @@ file.conversions <- function()
                                  ".csv", sep = ""), row.names = FALSE)
     stations[i, 1:3] <- data[1, 1:3]
     stations[i, 4:6] <- data[1, 8:10]
-    
+
   }
-  assign("stations", stations, envir = .GlobalEnv) 
+  assign("stations", stations, envir = .GlobalEnv)
   file.remove(files)
 } # End Function
 file.conversions()
@@ -175,19 +186,19 @@ for (i in 1:nrow(count(substr(files, 1, nchar(files)-9))))
   counts <- as.vector(count(substr(files, 1, nchar(files)-9))[,2])
   beg.count <- sum(counts[1:(i-1)])+1
   end.count <- sum(counts[1:i])
-  
+
   if(i == 1) {start <- 1} else {start <- beg.count}
-  
+
   station.data <- lapply(files[start:end.count],
                          read.csv, header = TRUE)
-  
+
   combined.df <- do.call(rbind , station.data)
-  
+
   assign(as.character(count(substr(files, 1, nchar(files)-9))[i,1]), combined.df)
-  
-  write.csv(combined.df, 
+
+  write.csv(combined.df,
             paste(as.character(count(substr(files, 1, nchar(files)-9))[i,1]),".csv",sep=""))
-  
+
   file.remove(files[start:end.count])
 }
 
